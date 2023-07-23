72b2ca76b20b18b3903ae5df9b3b71552a1c4de6
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-07-23 23:19:54 +0300

    all

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Starts a Flask web application
-"""
+'''A simple Flask web application.
+'''
 from flask import Flask
 
 
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
-    """Display 'Hello HBNB!'
-    """
+    '''The home page.'''
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
